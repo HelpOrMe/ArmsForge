59f7dfc04825d6890f351f095ab3770b95404858
HelpOrMe <31995593+HelpOrMe@users.noreply.github.com>
2020-07-26 19:38:28 +0300

    Add creative tab

---
+++ src/main/java/helporme/armsforge/common/core/tab/ArmsForgeTab.java
@@ -0,0 +1,26 @@
+package helporme.armsforge.common.core.tab;
+
+import helporme.armsforge.common.block.registry.BlocksRegistry;
+import helporme.armsforge.common.core.Version;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class ArmsForgeTab extends CreativeTabs
+{
+    public ArmsForgeTab()
+    {
+        super(Version.modid);
+    }
+
+    @Override
+    public Item getTabIconItem()
+    {
+        return Item.getItemFromBlock(BlocksRegistry.getBlockByName("MasterAnvil"));
+    }
+
+    @Override
+    public String getTranslatedTabLabel()
+    {
+        return Version.name;
+    }
+}

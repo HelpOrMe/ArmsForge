c58ee28d8c3849d7143d6f9223a46d6209c57774
HelpOrMe <31995593+HelpOrMe@users.noreply.github.com>
2020-08-13 01:15:21 +0300

    Fix sword equipped rotation

--- src/main/java/helporme/armsforge/client/render/items/ItemWeaponRenderer.java
+++ src/main/java/helporme/armsforge/client/render/items/ItemWeaponRenderer.java
@@ -34,6 +34,7 @@ public class ItemWeaponRenderer extends ItemRendererBase
         GL11.glRotatef(-45, 0, 1, 0);
         GL11.glRotatef(60, 0, 0, 1);
         GL11.glTranslatef(1.1f, -1f, 0);
+        GL11.glRotatef(180, 0, 1, 0);
         GL11.glScalef(2, 2, 2);
         renderModel();
         GL11.glPopMatrix();
@@ -61,7 +62,6 @@ public class ItemWeaponRenderer extends ItemRendererBase
         GL11.glRotatef(-45, 1, 0, 0);
         GL11.glRotatef(-45, 0, 0, 1);
         GL11.glTranslatef(-0.27f, -0.27f, 0);
-//        GL11.glScalef(2.5f, 2.5f, 2.5f);
         renderModel();
         GL11.glPopMatrix();
     }
@@ -74,7 +74,6 @@ public class ItemWeaponRenderer extends ItemRendererBase
         GL11.glRotatef(90, 0, 1, 0);
         GL11.glRotatef(-45, 0, 0, 1);
         GL11.glTranslatef(0, -0.4f, 0);
-//        GL11.glScalef(1.6f, 1.6f, 1.6f);
         renderModel();
         GL11.glPopMatrix();
     }
